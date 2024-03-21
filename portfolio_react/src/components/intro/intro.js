9d32f99788f21d25a6f963884642ac9d4bb4a7e9
Irungu Macharia <machariajosphat001@gmail.com>
2024-03-21 18:13:52 +0000

    aesthetic tweaks and improvements v1

--- portfolio_react/src/components/intro/intro.js
+++ portfolio_react/src/components/intro/intro.js
@@ -11,11 +11,11 @@ const Intro = () => {
         <div className='introContent'>
             <span className='hello'>Hello, </span>
             <span className='introText'>I'm <span className='introName'>Mr Mvshv</span><br />
-            <a href="https://git.io/typing-svg"><img src="https://readme-typing-svg.herokuapp.com?font=Fira+Code&pause=1000&random=false&width=435&lines=all+round+dope+guy" alt="Typing SVG" /></a>
+            <a href="https://git.io/typing-svg"><img src="https://readme-typing-svg.herokuapp.com?font=Fira+Code&weight=400&size=24&pause=1000&random=false&width=435&lines=all+round+dope+guy" alt="Typing SVG" /></a>
             </span>
             <p className='introPara'>
               <a href="https://git.io/typing-svg">
-                <img src="https://readme-typing-svg.herokuapp.com?font=Fira+Code&pause=500&random=True&width=420&multiline=true&lines=I+am+a+skilled+person+with+practise;f***ing+around+and+finding+out" alt="Typing SVG" />
+                <img src="https://readme-typing-svg.herokuapp.com?font=Fira+Code&pause=500&random=True&height=70&width=500&multiline=true&weight=420&size=22&lines=I+am+a+skilled+person+with+practise;f***ing+around+and+finding+out" alt="Typing SVG" />
               </a>
             </p>
             <Link>
